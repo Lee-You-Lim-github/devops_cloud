c1fe23ca3a48df03287f5305be8f8678d3e753a6
Lee-You-Lim-github <akfl513@naver.com>
2021-12-01 09:15:47 +0900

    장고 과제

---
+++ myhomework11/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):   #함수명을 PostAdmin라고 지은 이유 Post와 admin을 연결시키기 위해
+    list_display = ["title", "author_name", "created_at"]
+    search_fields = ["title"]
+
+admin.site.register(Post, PostAdmin)

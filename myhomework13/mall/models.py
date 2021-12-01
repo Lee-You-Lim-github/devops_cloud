720fa4b15de8716b6923d9f6d8a89d3f9e73ed57
Lee-You-Lim-github <akfl513@naver.com>
2021-12-01 17:57:13 +0900

    mall/Shop에 telephone 필드 추가

--- myhomework13/mall/models.py
+++ myhomework13/mall/models.py
@@ -4,6 +4,7 @@ class Shop(models.Model):
     # id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=13)
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

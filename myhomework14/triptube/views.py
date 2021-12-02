bfd1a719d0c530606da0c6cf02c7ff06f842aa7a
Lee-You-Lim-github <akfl513@naver.com>
2021-12-03 06:56:53 +0900

    CBV 예시 추가 - 장고 class!! 한 줄로 간단하게 할 수 있음! 지금은 class 함수로!

--- myhomework14/triptube/views.py
+++ myhomework14/triptube/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from triptube.models import Video
+# from django.views.generic import ListView
 
+# Class Based View (CBV)
+# index = ListView.as_view(model=Video, template_name="catube/index.html")
 
+# Function Based View (FBV)
 def index(request: HttpRequest) -> HttpResponse:
     qs = Video.objects.all()
     return render(

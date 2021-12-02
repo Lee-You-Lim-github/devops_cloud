cc8e3d5333d398dbd4d6a986f2e0f5607cf6055f
Lee-You-Lim-github <akfl513@naver.com>
2021-12-03 05:44:49 +0900

    catube 구현 중. + views에 따옴표 오류 해결/ 여태 index.html이 안 된 이유가 migrate를 안 해서 같음.

--- myhomework14/triptube/views.py
+++ myhomework14/triptube/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from triptube.models import Video
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "triptube/index.html")
+    qs = Video.objects.all()
+    return render(
+        request,
+        "triptube/index.html",
+        {
+            "video_list": qs,
+        },
+    )
+

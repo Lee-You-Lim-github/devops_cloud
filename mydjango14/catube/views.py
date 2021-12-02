b3a227ac91f4614b776752ee69d2f1179c8e23d3
Lee-You-Lim-github <akfl513@naver.com>
2021-12-02 11:09:26 +0900

    mall/index 뷰 구현

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "catube/index.html")  #앱 이름을 써 줌. -> 어떤 뷰에서 사용하는 구나.

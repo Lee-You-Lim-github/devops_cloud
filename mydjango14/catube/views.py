6c0157e120d8b196cb0ce3121acc172307d9148c
Lee-You-Lim-github <akfl513@naver.com>
2021-12-02 15:16:56 +0900

    비디오 재생 기능 추가

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -10,3 +10,8 @@ def index(request: HttpRequest) -> HttpResponse:
         "catube/index.html",
         {"video_list": qs},
     )  # 앱 이름을 써 줌. -> 어떤 뷰에서 사용하는 구나.
+
+
+def video_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    video = Video.objects.get(pk=pk)
+    return render(request, "catube/vedeo_detail.html", {"video": video})

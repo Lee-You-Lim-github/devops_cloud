60d1819900905a5d8960602adf6bf2aab07d8180
Lee-You-Lim-github <akfl513@naver.com>
2021-12-02 14:54:36 +0900

    catube 구현 중.

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from catube.models import Video
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "catube/index.html")  #앱 이름을 써 줌. -> 어떤 뷰에서 사용하는 구나.
+    qs = Video.objects.all()
+    return render(
+        request,
+        "catube/index.html",
+        {"video_list": qs},
+    )  # 앱 이름을 써 줌. -> 어떤 뷰에서 사용하는 구나.

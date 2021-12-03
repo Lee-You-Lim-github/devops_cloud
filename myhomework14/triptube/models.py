456ece9dc48abace0a89f0699ed5699bc031cedd
Lee-You-Lim-github <akfl513@naver.com>
2021-12-03 10:10:07 +0900

    bootstrap5 top navigation 예제 적용 - models에 오타가 2개나 있었음!!!

--- myhomework14/triptube/models.py
+++ myhomework14/triptube/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
 
 class Video(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     video_file = models.FileField()
     thumbnail_file = models.ImageField()
+    thumbnail_file_thumb = ImageSpecField(
+        source="thumbnail_file",
+        processors=[ResizeToFill(800, 400)],
+        format = "JPEG",
+        options= {"quality": 60},
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

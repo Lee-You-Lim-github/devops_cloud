603a125c3f49160e5219b61a4e74682d4e992d8b
Lee-You-Lim-github <akfl513@naver.com>
2021-12-03 13:57:13 +0900

    admin 설정 후 views에서 함수를 만들고 html를 짠 뒤에 urls 함.

--- mydjango15/delicious/views.py
+++ mydjango15/delicious/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpRequest,HttpResponse
 from django.shortcuts import render
+from delicious.models import Shop
 
-# Create your views here.
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    # template_name = "delicious/shop_list.html"    # 장고에서 사용하고 있는 정식 명칭
+    context_data = {
+        "shop_list": qs,
+    }                             # 장고에서 사용하고 있는 정식 명칭
+    return render(request, "delicious/shop_list.html", context_data)
+
+
+def shop_detail(request: HttpRequest, pk:int) -> HttpResponse:
+    shop = Shop.objects.get(pk=pk)    # pk=pk(변수명=필드값)
+    # template_name = "delicious/shop_detail.html"
+    context_date = {
+        "shop":shop,
+    }
+    return render(request, "delicious/shop_detail.html", context_date)

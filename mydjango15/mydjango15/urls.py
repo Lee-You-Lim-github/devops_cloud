4c5bb284b65fd1e3d7465d4b04438fb996bc16d0
Lee-You-Lim-github <akfl513@naver.com>
2021-12-06 05:49:58 +0900

    urls 오류 고침

--- mydjango15/mydjango15/urls.py
+++ mydjango15/mydjango15/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from mydjango15 import settings
+from django.conf import settings
 from delicious.views import shop_list, shop_detail, shop_new_1, shop_new
 
 urlpatterns = [

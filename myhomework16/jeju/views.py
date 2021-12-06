c59f9355508e6fbab9c3640ffc9e517f1ad84c36
Lee-You-Lim-github <akfl513@naver.com>
2021-12-07 05:57:58 +0900

    Bootstrap5 CDN 적용하여 layout과 tour_list를 구현

--- myhomework16/jeju/views.py
+++ myhomework16/jeju/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from jeju.models import Tour
 
-# Create your views here.
+
+def tour_list(request: HttpRequest) -> HttpResponse:
+    qs = Tour.objects.all()
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(name__icontains=query)
+    context_data = {
+        "tour_list": qs
+    }
+    return render(request, "jeju/tour_list.html", context_data)
--- myhomework16/myhomework16/urls.py
+++ myhomework16/myhomework16/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from jeju.views import tour_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jeju/', tour_list),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,

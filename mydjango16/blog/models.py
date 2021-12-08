0239ca1126c5d7d8bef0efb16fc6d6fee5094420
Lee-You-Lim-github <akfl513@naver.com>
2021-12-08 16:52:57 +0900

    1:N/M:N 관계 모델 및 마이그레이션 추가

--- mydjango16/blog/models.py
+++ mydjango16/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):    # pk: id(int)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)    # Post를 지울 때, 댓글(comment)도 같이 지워줌.
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

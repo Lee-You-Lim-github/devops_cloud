7b4f2d11ae19244279bb2db0fe6a79f1eedbcd64
Lee-You-Lim-github <akfl513@naver.com>
2021-12-08 17:30:52 +0900

    M:N를 활용한 Tag 모델 설정

--- mydjango16/blog/admin.py
+++ mydjango16/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post, Comment
+from blog.models import Post, Comment, Tag
 
 @admin.register(Post)      # admin.site.register(Post, PostAdmin)
 class PostAdmin(admin.ModelAdmin):
@@ -11,3 +11,6 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ["pk", "post", "message", "created_at"]
 
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- mydjango16/blog/models.py
+++ mydjango16/blog/models.py
@@ -3,8 +3,16 @@ from django.db import models
 class Post(models.Model):    # pk: id(int)
     title = models.CharField(max_length=200, db_index=True)
     content = models.TextField()
+    tag_set = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # 인스턴스에 대한 문자열 포현을 기대
+    # post.title
+    # print(post)    # 내부적으로 post.__str__을 찾아서 호출해서 반환값을 출력
+    # print(post.__str__())과 같은.
+    def __str__(self) -> str:
+        return self.title
 
 
 class Comment(models.Model):
@@ -12,3 +20,9 @@ class Comment(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)    # 같은 Tag가 있을 시 검색 X -> unique 해야 함.
+
+    def __str__(self) -> str:
+        return self.name

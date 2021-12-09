8a53f5c9ec2471373cab00c4671940dd4b6d3ad4
Lee-You-Lim-github <akfl513@naver.com>
2021-12-09 18:04:03 +0900

    post_list 뷰 구현

--- mydjango17/diary/views.py
+++ mydjango17/diary/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from diary.models import Post
 
-# Create your views here.
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()    # 전체 포스팅 목록을 얻어올 준비, 요청이 들어오기 전까지 DB에 접속 X
+    return render(request, "diary/post_list.html", {
+        "post_list": qs,
+    })
+

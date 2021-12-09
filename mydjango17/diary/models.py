29b7db3248bde6baa7393ab3314e8e8f54681381
Lee-You-Lim-github <akfl513@naver.com>
2021-12-09 17:47:53 +0900

    diary 모델/마이그레이션 생성 및 admin에 등록, updated_at 수정

--- mydjango17/diary/models.py
+++ mydjango17/diary/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True   # 추상 클래스로서, created_at, created_at의 DB 테이블이 생기지 않습니다.
+
+class Post(TimestampedModel):
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=200, db_index=True)  # db_index=True: 검색 기능을 위해
+    content = models.TextField()
+    photo = models.ImageField(upload_to="diary/post/%Y/%m/%d")
+    tag_set = models.ManyToManyField("Tag", blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "포스팅"
+        verbose_name_plural = "포스팅 목록"
+
+class Comment(TimestampedModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # on_delete=models.CASCADE: 포스팅을 지우면 댓글도 같이 지워 줌.
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name = "댓글"
+        verbose_name_plural = "댓글 목록"
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=200, db_index=True)  # db_index=True: 검색 기능을 위해
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "태그"
+        verbose_name_plural = "태그 목록"

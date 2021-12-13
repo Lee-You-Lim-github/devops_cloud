da7c554a1b170720e62ccc3a568be36420d1d76f
Lee-You-Lim-github <akfl513@naver.com>
2021-12-13 23:44:24 +0900

    layout과 shop_list 구현

--- myhomework21_3/shop/views.py
+++ myhomework21_3/shop/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+# /shop/
+from shop.models import Category, Shop
+
+
+def shop_list(request:HttpRequest) -> HttpResponse:
+    category_qs = Category.objects.all()
+    qs = Shop.objects.all()
+
+    category_id = request.GET.get("category_id", "")
+    if category_id:
+        qs = qs.filter(category__pk=category_id)
+
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(name__icontains=query)
+
+    return render(request, "shop/shop_list.html", {
+        "shop_list": qs,
+        "category_list": category_qs,
+    })

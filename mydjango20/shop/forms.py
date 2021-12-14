546741ada992a11cb4b1f59be9a4a183e2bd687b
Lee-You-Lim-github <akfl513@naver.com>
2021-12-14 14:11:34 +0900

    !!admin에서의 새 포스팅 쓰기 수정!!

--- mydjango20/shop/forms.py
+++ mydjango20/shop/forms.py
@@ -8,10 +8,14 @@ class ShopForm(forms.ModelForm):
     # 부모 클래스의 생성자에서 어떤 인자를 지원하는 지는 잘 모르겠지만,
     # 생성자 호출 시에 받을 인자 그대로
     # 부모에게 전달하겠다.
+
+# 가지고 있던 태그값 호출
     def __init__(self, *args, **kwargs):  # 받을 시: pack
         super().__init__(*args, **kwargs) # 호출 시: unpack
         # tags는 우리가 Form 클래스에 직접 추가한 필드니깐
         # 초기값도 우리가 직접 지정해주어야 합니다.
+
+# 초기값 기억하기
         if self.instance.pk: # 수정 시
             tag_qs = self.instance.tag_set.all()
             tags = ", ".join([tag.name for tag in tag_qs])
@@ -19,9 +23,10 @@ class ShopForm(forms.ModelForm):
         # else:   # 새롭게 생성 시
         #     pass
 
-    def save(self):
+# 폼내부에서 태그입력가능
+    def _save_m2m(self):
         # 부모의 save를 호출해 주어야 함.         # 부모의 save 호출 X
-        saved_post = super().save()
+        super()._save_m2m()
 
         # 부가적인 연산을 수행할 수 있음.
         tag_list = []
@@ -31,10 +36,9 @@ class ShopForm(forms.ModelForm):
             tag, __ = Tag.objects.get_or_create(name=tag_name)
             tag_list.append(tag)
 
-        saved_post.tag_set.clear()  # 간단구현을 위해 clear 호출
-        saved_post.tag_set.add(*tag_list)
+        self.instance.tag_set.clear()  # 간단구현을 위해 clear 호출
+        self.instance.tag_set.add(*tag_list)
 
-        return saved_post
 
 
     class Meta:

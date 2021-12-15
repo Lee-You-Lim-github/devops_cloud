4255214f8c8523f514faf2f3f09372712caadba7
Lee-You-Lim-github <akfl513@naver.com>
2021-12-15 13:19:08 +0900

    layout과 blog 앱 post_list/post_detail/post_new/post_edit 뷰 기본 구현

--- mydjango23/blog/views.py
+++ mydjango23/blog/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from blog.forms import PostForm
+from blog.models import Post
+
+
+def post_list(request:HttpRequest) -> HttpResponse:
+    post_qs = Post.objects.all()
+    return render(request, "blog/post_list.html", {
+        'post_list':post_qs,
+    })
+
+
+def post_detail(request:HttpRequest, pk:int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+    })
+
+
+def post_new(request:HttpRequest) -> HttpResponse:
+    # request.method  # "GET", "POST"가 들어있어야 함.
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            saved_post = form.save()
+            messages.success(request, "새로운 포스팅을 저장했습니다.")
+            return redirect("blog:post_detail", saved_post.pk)    # saved_post = form.save()에 pk값이 저장되었기 때문에 saved_post.pk라고 해야 함.
+    else:
+        form = PostForm()
+    return render(request, "blog/post_form.html", {
+        "form": form,
+    })
+
+def post_edit(request:HttpRequest, pk:int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=pk)   # 수정 대상을 불러옴.
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            saved_post = form.save()
+            messages.success(request, f"{pk} 포스팅을 저장했습니다.")
+            return redirect("blog:post_detail",
+                            saved_post.pk)  # saved_post = form.save()에 pk값이 저장되었기 때문에 saved_post.pk라고 해야 함.
+    else:
+        form = PostForm(instance=post)
+    return render(request, "blog/post_form.html", {
+        "form": form,
+        "post": post,
+    })
+
+def post_delete(request:HttpRequest, pk:int) -> HttpResponse:
+    raise NotImplementedError("삭제는 아직 강의에서 다루지 않았습니다.")

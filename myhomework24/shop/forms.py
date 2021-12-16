4416d99a46a3b7f4725cc2b4cec154a1a07533bf
Lee-You-Lim-github <akfl513@naver.com>
2021-12-16 19:47:25 +0900

    뷰(reverse 미적용), 폼 구현 및 shop/url 설정

---
+++ myhomework24/shop/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from shop.models import Post
+
+
+class PostForm(forms.ModelForm):  # 모델폼을 상속 받음.
+    class Meta:
+        model = Post,
+        fields = "__all__"
--- myhomework24/shop/views.py
+++ myhomework24/shop/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from shop.forms import PostForm
+from shop.models import Post, Category, Review, Tag
+
+# post/
+post_list = ListView.as_view(
+    model=Post,
+)
+
+# post/<int:pk>
+post_detail = DetailView.as_view(
+    model=Post,
+)
+
+# post/new
+post_new = CreateView.as_view(
+    model=Post,
+    form_class=PostForm,
+)
+
+# post/<int:pk>/edit
+post_edit = UpdateView.as_view(
+    model=Post,
+    form_class=PostForm,
+)
+
+# post/<int:pk>/delete
+post_delete = DeleteView.as_view(
+    model=Post,
+)
+
+
+
+

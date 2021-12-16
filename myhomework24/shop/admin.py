2df22339cd231495f36baf9332270f1b6227ffd9
Lee-You-Lim-github <akfl513@naver.com>
2021-12-16 18:55:01 +0900

    Category, Post, Review, Tag 모델 만들고 admin 에 e등록 -__str__, Meta.ordering 지정

--- myhomework24/shop/admin.py
+++ myhomework24/shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Category, Post, Review, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- myhomework24/shop/models.py
+++ myhomework24/shop/models.py
@@ -1,3 +1,69 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModels(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimestampedModels):
+    name = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+
+class Post(TimestampedModels):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='shop/post/%y/%m/%d')
+    telephone = models.CharField(max_length=14,
+                                 validators=[
+                                RegexValidator(r"^\d{0,4}-?\d{3,4}-?\d{4}$")
+                                 ])
+    gender = models.CharField(
+        max_length=1,
+    choices=[
+        ('M', '남자'),
+        ('F', '여자'),
+        ('N', '기타')],
+        db_index=True,
+        default='N',
+    )
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.title
+
+class Review(TimestampedModels):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=100)
+    message = models.TextField()
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.author_name
+
+
+class Tag(TimestampedModels):
+    name = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name

eb96fde1bc1b3e85d589c8825469c5360904e1ae
Lee-You-Lim-github <akfl513@naver.com>
2021-12-17 14:33:06 +0900

    shop_list cbv로 구현 및 category_list 추가

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from shop.models import Shop, Category
+
+
+class ShopListView(ListView):
+    model = Shop   # 나머지는 옵션 - default 값으로 지정됨.
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["category_list"] = Category.objects.all()
+        return context_data
+
+# shop/
+shop_list = ShopListView.as_view()

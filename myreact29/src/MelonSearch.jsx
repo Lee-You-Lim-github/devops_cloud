69204a9f8f20a24eebfa381a19e3c3a8d3d7ad42
Lee-You-Lim-github <akfl513@naver.com>
2021-12-24 07:55:54 +0900

    과제: antd를 이용한 list 디자인 변경

--- myreact29/src/MelonSearch.jsx
+++ myreact29/src/MelonSearch.jsx
@@ -65,10 +65,19 @@ function MelonSearch() {
       />
       {songList.map((song) => {
         return (
-          <div key={song.SONGID}>
-            <img src={song.ALBUMIMG} />
-            {song.SONGNAME} by {song.ARTISTNAME}
-          </div>
+          <List
+            itemLayout="horizontal"
+            dataSource={songList}
+            renderItem={(song) => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar src={song.ALBUMIMG} />}
+                  title={song.SONGNAME}
+                  description={song.ARTISTNAME}
+                />
+              </List.Item>
+            )}
+          />
         );
       })}
     </div>

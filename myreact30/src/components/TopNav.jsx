91c8c52a00d20d5bf196130972c98e1c04ad2242
Lee-You-Lim-github <akfl513@naver.com>
2021-12-27 05:25:17 +0900

    TopNav 컴포넌트 구현

---
+++ myreact30/src/components/TopNav.jsx
@@ -0,0 +1,15 @@
+// 스타일링
+function TopNav({ changePageName }) {
+  return (
+    <ul>
+      <li>
+        <a onClick={() => changePageName("about")}>About</a>
+      </li>
+      <li>
+        <a onClick={() => changePageName("counter")}>Counter</a>
+      </li>
+    </ul>
+  );
+}
+
+export default TopNav;

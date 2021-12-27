f47db91c26557580da2a9e569aeb632a4917e607
Lee-You-Lim-github <akfl513@naver.com>
2021-12-27 12:52:09 +0900

    PageLotto 컴포넌트에서 하나의 상탯값을 정의. 그 상탯값의 기본값은 배열로서 [10, 11, 12, 13, 14, 15, 16, 17] 값을 줍니다. 이 상탯값의 각 숫자를 PageLotto 컴포넌트에서 표현합니다. + style

--- myreact31/src/pages/PageLotto.jsx
+++ myreact31/src/pages/PageLotto.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 function PageLotto() {
-  return <h2>Lotto</h2>;
+  const [numbers, setNumbers] = useState([10, 11, 12, 13, 14, 15, 16]);
+
+  return (
+    <div>
+      <h2>Lotto</h2>
+
+      {numbers.map((number) => (
+        <span style={{ display: "inline-block", margin: "1rem" }}>
+          {number}
+        </span>
+      ))}
+    </div>
+  );
 }
 
 export default PageLotto;

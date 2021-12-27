51666b1d6a2a72793c272f675b7634e0da8be307
Lee-You-Lim-github <akfl513@naver.com>
2021-12-27 16:34:10 +0900

    로또 번호 7자리 호출

--- myreact31/src/pages/PageLotto.jsx
+++ myreact31/src/pages/PageLotto.jsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
 
 function PageLotto() {
+  // 숫자 배열
   const [numbers, setNumbers] = useState([10, 11, 12, 13, 14, 15, 16]);
+
+  // 로또 번호 7자리
+
+  function makeLottoNumbers() {
+    const lottoArray = [...Array(45).keys()];
+
+    const lottoNumbers = lottoArray
+      .map((number) => ({
+        number,
+        random: Math.random(),
+      }))
+      .sort((a, b) => a.random - b.random)
+      .slice(0, 7)
+      .map(({ number }) => number + 1)
+      .sort((num1, num2) => num1 - num2);
+    return lottoNumbers;
+  }
+
+  function handleclick() {
+    console.log(makeLottoNumbers());
+    return setNumbers(makeLottoNumbers());
+  }
 
   return (
     <div>
       <h2>Lotto</h2>
-
-      {numbers.map((number) => (
+      <button onClick={handleclick}>예지</button>
+      {numbers.map((lottoNumber) => (
         <span style={{ display: "inline-block", margin: "1rem" }}>
-          {number}
+          {lottoNumber}
         </span>
       ))}
     </div>

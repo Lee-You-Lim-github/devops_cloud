3c34f758ab1288eff48d34c243f6de34224b69e1
Lee-You-Lim-github <akfl513@naver.com>
2021-12-29 16:21:22 +0900

    map을 이용하여 onClick의 반복을 줄임.

--- myreact31/src/App.js
+++ myreact31/src/App.js
@@ -2,31 +2,30 @@ import PageLotto from "./pages/PageLotto";
 import ProfileCard from "./components/ProfileCard";
 import { useState } from "react";
 import TopNav from "components/TopNav";
-import ProfileList from "./data/ProfileList.json";
+import profileList from "./data/ProfileList.json";
 
 function App() {
   const [pageName, setPageName] = useState("user1");
-  const [selectProfileName, setSelectProfileName] = useState(ProfileList[0].id);
+  const [selectProfileName, setSelectProfileName] = useState(profileList[0].id);
 
   return (
     <div>
       <TopNav changePageName={setPageName} />
       {pageName === "lotto" && <PageLotto />}
       <hr />
-      {ProfileList.map((profile, index) => {
+      {profileList.map((profile, index) => {
         if (selectProfileName === profile.id) {
           return (
             <div key={profile.id} className={`member${(index % 4) + 1}`}>
               <ProfileCard {...profile}>
-                <a
-                  onClick={() => setSelectProfileName("user1")}
-                  className="on"
-                ></a>
-                <a onClick={() => setSelectProfileName("user2")}></a>
-                <a onClick={() => setSelectProfileName("user3")}></a>
-                <a onClick={() => setSelectProfileName("user4")}></a>
-                <a onClick={() => setSelectProfileName("user5")}></a>
-                <a onClick={() => setSelectProfileName("user6")}></a>
+                {profileList.map((click) => {
+                  return (
+                    <a
+                      onClick={() => setSelectProfileName(click.id)}
+                      className="on"
+                    ></a>
+                  );
+                })}
               </ProfileCard>
             </div>
           );

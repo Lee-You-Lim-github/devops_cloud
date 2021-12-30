6deabb00aa2b9577639a54779b99ab9973b111e0
Lee-You-Lim-github <akfl513@naver.com>
2021-12-30 13:29:11 +0900

    useState()에 초기값을 객체가 있는 배열로 지정함.

--- myreact34/src/pages/PageProfile.jsx
+++ myreact34/src/pages/PageProfile.jsx
@@ -1,30 +1,41 @@
 import { useState } from 'react';
 
 function PageProfile() {
-  const [profileList, setProfileList] = useState({
-    uniqueid: 'bts-jin',
-    name: '진',
-    role: '서브보컬',
-    mbit: 'INFP',
-    instagramUrl: 'https://instagram.com/jin',
-    profileImageUrl:
-      'https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg',
-  });
+  const [profileList, setProfileList] = useState([
+    {
+      uniqueid: 'bts-jin',
+      name: '진',
+      role: '서브보컬',
+      mbit: 'INFP',
+      instagramUrl: 'https://instagram.com/jin',
+      profileImageUrl:
+        'https://classdevopscloud.blob.core.windows.net/data/bts-jin.jpg',
+    },
+  ]);
+
+  //   useEffect(() => {
+
+  //   }, []);
 
   return (
     <div>
       <h2>PageProfile</h2>
-      <div>
-        <section>
-          <ul>
-            <h3>{profileList.name}</h3>
-            <li>{profileList.mbit}</li>
-            <li>{profileList.role}</li>
-            <li>인스타그램 주소:{profileList.instagramUrl}</li>
-            <img src={profileList.profileImageUrl} />
-          </ul>
-        </section>
-      </div>
+
+      {profileList.map((profile) => {
+        return (
+          <div key={profile.uniqueid}>
+            <section>
+              <ul>
+                <h3>{profile.name}</h3>
+                <li>{profile.mbit}</li>
+                <li>{profile.role}</li>
+                <li>인스타그램 주소:{profile.instagramUrl}</li>
+                <img src={profile.profileImageUrl} />
+              </ul>
+            </section>
+          </div>
+        );
+      })}
     </div>
   );
 }

31b64a70e1e20362ce74196c8468f5d01cd79a05
Lee-You-Lim-github <akfl513@naver.com>
2021-12-30 16:10:24 +0900

    useEffect를 활용하여 json을 가져오고 map으로 필드명을 바꿔줌.

--- myreact34/src/pages/PageProfile.jsx
+++ myreact34/src/pages/PageProfile.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function PageProfile() {
   const [profileList, setProfileList] = useState([]);
+
+  useEffect(() => {
+    Axios.get(
+      'https://classdevopscloud.blob.core.windows.net/data/profile-list.json',
+    )
+
+      .then((response) => {
+        const bts = response.data.map((axios_profile) => ({
+          name: axios_profile.name,
+          role: axios_profile.role,
+          mbti: axios_profile.mbti,
+          uniqueId: axios_profile.unique_id,
+          instagramUrl: axios_profile.instagram_url,
+          profileImageUrl: axios_profile.profile_image_url,
+        }));
+        setProfileList(bts);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div>
@@ -9,12 +31,12 @@ function PageProfile() {
       {profileList.length === 0 && <h1>등록된 프로필이 없습니다.</h1>}
       {profileList.map((profile) => {
         return (
-          <div key={profile.uniqueid}>
+          <div key={profile.uniqueId}>
             <section>
               <ul>
                 <h3>{profile.name}</h3>
-                <li>{profile.mbit}</li>
                 <li>{profile.role}</li>
+                <li>{profile.mbti}</li>
                 <li>인스타그램 주소:{profile.instagramUrl}</li>
                 <img src={profile.profileImageUrl} />
               </ul>

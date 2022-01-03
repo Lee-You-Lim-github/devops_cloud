22a81ddd76c14e48973fec53e750fdce884bd76c
Lee-You-Lim-github <akfl513@naver.com>
2022-01-03 13:36:53 +0900

    setColor에 직접적으로 값 넘김

--- myreact35/src/Counter3.jsx
+++ myreact35/src/Counter3.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function Counter3() {
   const [value, setValue] = useState(0);
+  const [color, setColor] = useState("red");
 
   // const handlePlus = () => {
   //   return setValue(value + 1);
@@ -19,15 +20,43 @@ function Counter3() {
     setValue((prevValue) => prevValue - 1);
   };
 
+  // COLOR: ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  const handleGreen = () => {
+    return setColor("green");
+  };
+
+  const handleBlue = () => {
+    return setColor("blue");
+  };
+
+  const handleRed = () => {
+    return setColor("red");
+  };
+
   return (
     <div>
       <h2>Counter3</h2>
-      {value}
+      <div style={{ ...defaultStyle, backgroundColor: color }}>{value}</div>
       <hr />
       <button onClick={handlePlus}>증가</button>
       <button onClick={handleMinus}>감소</button>
+      <hr />
+      <button onClick={handleGreen}>green</button>
+      <button onClick={handleBlue}>blue</button>
+      <button onClick={handleRed}>red</button>
     </div>
   );
 }
+
+const defaultStyle = {
+  width: "100px",
+  height: "100px",
+  borderRadius: "50px",
+  lineHeight: "100px",
+  textAlign: "center",
+  display: "inline-block",
+  fontSize: "3rem",
+  userSelect: "none",
+};
 
 export default Counter3;

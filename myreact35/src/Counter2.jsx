e71d2c047298b5774166b65f241bc862b099e49f
Lee-You-Lim-github <akfl513@naver.com>
2022-01-03 13:05:55 +0900

    reduce 함수를 활용한 색 변경

--- myreact35/src/Counter2.jsx
+++ myreact35/src/Counter2.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 
+function reducer_color(action, prevColor) {
+  const { type, color } = action;
+  if (type === "CHANGE_COLOR") {
+    prevColor = color;
+    console.log(prevColor);
+    return prevColor;
+  }
+}
+
 // dispatch 방식
 // { type: "PLUS", amount: 1 }
 // { type: "MINUS", amount: 1 }
-function dispatch(action, prevValue) {
+function reducer(action, prevValue) {
   // 순수함수로서 구현이 되어야만 합니다.
   // 같은 값의 인자가 주어지만, 항상 같은 값의 리턴을 해야만 하고
   // 인자 외에 다른 어떤 값, 어떤 통신을 해서도 안 됩니다.
@@ -19,30 +28,66 @@ function dispatch(action, prevValue) {
 
 function Counter2() {
   const [value, setValue] = useState(0);
+  const [color, setColor] = useState("red");
 
   const handlePlus = () => {
     const action = { type: "PLUS", amount: 1 };
     setValue((prevValue) => {
-      return dispatch(action, prevValue);
+      return reducer(action, prevValue);
     });
   };
 
   const handleMinus = () => {
     const action = { type: "MINUS", amount: 1 };
     setValue((prevValue) => {
-      return dispatch(action, prevValue);
+      return reducer(action, prevValue);
+    });
+  };
+
+  // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  const handleGreen = () => {
+    const action = { type: "CHANGE_COLOR", color: "green" };
+    setColor((prevColor) => {
+      return reducer_color(action, prevColor);
+    });
+  };
+  const handleBlue = () => {
+    const action = { type: "CHANGE_COLOR", color: "blue" };
+    setColor((prevColor) => {
+      return reducer_color(action, prevColor);
+    });
+  };
+  const handleRed = () => {
+    const action = { type: "CHANGE_COLOR", color: "red" };
+    setColor((prevColor) => {
+      return reducer_color(action, prevColor);
     });
   };
 
   return (
     <div>
       <h1>Counter2</h1>
-      {value}
+      <div style={{ ...defaultStyle, backgroundColor: color }}>{value}</div>
       <hr />
       <button onClick={handlePlus}>증가</button>
       <button onClick={handleMinus}>감소</button>
+      <hr />
+      <button onClick={handleGreen}>green</button>
+      <button onClick={handleBlue}>blue</button>
+      <button onClick={handleRed}>red</button>
     </div>
   );
 }
+
+const defaultStyle = {
+  width: "100px",
+  height: "100px",
+  borderRadius: "50px",
+  lineHeight: "100px",
+  textAlign: "center",
+  display: "inline-block",
+  fontSize: "3rem",
+  userSelect: "none",
+};
 
 export default Counter2;

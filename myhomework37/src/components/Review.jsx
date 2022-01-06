e8db24b5dafde7a2cd93e13dd91020ee0a704a5c
Lee-You-Lim-github <akfl513@naver.com>
2022-01-06 13:33:45 +0900

    [과제 풀이 추가] Review 컴포넌트에 수정/삭제 버튼 추가

--- myhomework37/src/components/Review.jsx
+++ myhomework37/src/components/Review.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react/cjs/react.development";
 import Rating from "./Rating";
 
 function Review({ review }) {
+  const [showMenu, setShowMenu] = useState(false);
   const score = review.score;
 
   return (
     <div
+      onMouseEnter={() => setShowMenu(true)}
+      onMouseLeave={() => setShowMenu(false)}
       className="
     border-2 
     border-green-500 
     m-1 
-    p-1 
-    hover:bg-green-100 
-    cursor-pointer"
+    p-1
+    pt-5 
+    
+    relative
+    "
     >
+      {showMenu && (
+        <div className="text-xs absolute top-0.5 right-1">
+          <span className="text-green-900 hover:text-green-600 cursor-pointer mr-1">
+            수정
+          </span>
+          <span className="text-red-700 hover:text-red-600 cursor-pointer">
+            삭제
+          </span>
+        </div>
+      )}
       {review.text}
       <Rating score={score} />
     </div>
